291222988b9b50c2ea02f26fe9a1ff2b118bc23d
ccbrown <ccbrown112@gmail.com>
2013-09-24 19:15:03 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-type: text/html;charset=utf-8');
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		
+		<title>Final Fantasy | ファイナルファンタジ &lt;&lt;FFXIV&gt;&gt;</title>
+		<link rel="stylesheet" type="text/css" href="style/style.css" />
+	</head>
+	<body>
+		<div id="contentbox">				
+			<div id="content">
+				<center>
+					<br /><br /><br />
+					<h1>準備中</h1>
+					<h1>Under Construction</h1>
+					<br />
+					<a href="forums"><b>The forums are up though!</b></a>
+				</center>
+			</div>
+		</div>
+	</body>
+</html>
