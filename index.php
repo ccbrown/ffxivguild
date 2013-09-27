49e4e63d21ed259fd98ef311112cf4e7571b2d27
ccbrown <ccbrown112@gmail.com>
2013-09-27 01:18:04 -0700

    home page styling

--- index.php
+++ index.php
@@ -21,17 +21,24 @@ require_once "./includes/common.inc.php";
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 		
 		<title>Final Fantasy | ファイナルファンタジ &lt;&lt;FFXIV&gt;&gt;</title>
+		
 		<link rel="stylesheet" type="text/css" href="style/style.css" />
+
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.ui.effect.min.js"></script>
 	</head>
 	<body>
+		<div id="bggradient">&nbsp;</div>
+	
 		<div id="contentbox">				
 			<div id="content">
 				<center>
-					<br /><br /><br />
+					<br />
 					<h1>準備中</h1>
 					<h1>Under Construction</h1>
 					<br />
 					<a href="forums"><b>The forums are up though!</b></a>
+					<br /><br />
 				</center>
 
 				<?php
@@ -48,11 +55,10 @@ require_once "./includes/common.inc.php";
 					$bbcode->bbcode_second_pass($post_text, $first_post['bbcode_uid'], $first_post['bbcode_bitfield']);
 					$post_text = smiley_text($post_text);
 					?>
-					<hr />
-					<div class="post">
-						<div class="title">
-							<a href="forums/viewtopic.php?f=<?= $_['phpbb']['news_forum_id'] ?>&t=<?= htmlspecialchars($topic['topic_id']) ?>"><?= $topic['topic_title'] ?></a>
-							<div class="author">Posted by <a href="forums/memberlist.php?mode=viewprofile&u=<?= htmlspecialchars($topic['topic_poster']) ?>"><?= htmlspecialchars($topic['topic_first_poster_name']) ?></a> at <?= create_date($topic['topic_time']) ?></div>
+					<div class="light">
+						<div class="head">
+							<a href="forums/viewtopic.php?f=<?= $_['phpbb']['news_forum_id'] ?>&t=<?= htmlspecialchars($topic['topic_id']) ?>"><h1><?= $topic['topic_title'] ?></h1></a>
+							<div class="subtitle">Posted by <a href="forums/memberlist.php?mode=viewprofile&u=<?= htmlspecialchars($topic['topic_poster']) ?>"><?= htmlspecialchars($topic['topic_first_poster_name']) ?></a> at <?= create_date($topic['topic_time']) ?></div>
 						</div>
 						<div class="body"><?= $post_text ?></div>
 					</div>
@@ -61,5 +67,18 @@ require_once "./includes/common.inc.php";
 				?>
 			</div>
 		</div>
+		
+		<script>
+		$(function() {
+			var colors = ['#000070', '#700070', '#700000', '#707000', '#007000', '#007070'];
+			var next = Math.floor(Math.random() * 6);
+			var doNextColorAnimation = function() {
+				$('body').animate({
+					'backgroundColor': colors[next++]
+				}, 5000, doNextColorAnimation);
+			}
+			doNextColorAnimation();
+		});
+		</script>
 	</body>
 </html>
