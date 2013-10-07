ac79348947aed28387d29ad8a3c129a3541a3a5b
ccbrown <ccbrown112@gmail.com>
2013-10-07 16:41:26 -0700

    ajaxify content, add placeholder application page

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
 header('Content-type: text/html;charset=utf-8');
-
-define('IN_PHPBB', true);
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './forums/';
-$phpEx = substr(strrchr(__FILE__, '.'), 1);
-include($phpbb_root_path . 'common.' . $phpEx);
-include($phpbb_root_path . 'includes/bbcode.' . $phpEx);
-include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
-
-$user->session_begin();
-$auth->acl($user->data);
-$user->setup();
-
-define('IN_FFXIVG', true);
-require_once "./includes/common.inc.php";
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,6 +8,7 @@ require_once "./includes/common.inc.php";
 		
 		<title>Final Fantasy | ファイナルファンタジ &lt;&lt;FFXIV&gt;&gt;</title>
 		
+		<link rel="stylesheet" type="text/css" href="style/ruby.css" />
 		<link rel="stylesheet" type="text/css" href="style/style.css" />
 
 		<script type="text/javascript" src="js/jquery.min.js"></script>
@@ -30,42 +17,96 @@ require_once "./includes/common.inc.php";
 	</head>
 	<body>
 		<div id="bggradient">&nbsp;</div>
-	
-		<div id="contentbox">				
-			<div id="content">
-				<center>
-					<br />
-					<h1>準備中</h1>
-					<h1>Under Construction</h1>
-					<br />
-					<a href="forums"><b>The forums are up though!</b></a>
-					<br /><br />
-				</center>
 
-				<?php
-				$result = $SQL->query("SELECT * FROM {$_['phpbb']['db_table_prefix']}topics WHERE forum_id = '".$SQL->escape_string($_['phpbb']['news_forum_id'])."' ORDER BY topic_time DESC LIMIT 0, 5");
-				while ($topic = $SQL->fetch_assoc($result)) {
-					$result2 = $SQL->query("SELECT * FROM {$_['phpbb']['db_table_prefix']}posts WHERE post_id = '".$SQL->escape_string($topic['topic_first_post_id'])."'");
-					$first_post = $SQL->fetch_assoc($result2);
+		<script>
+			$(function() {				
+				var loadingCounter = 0;
 				
-					$post_text = nl2br($first_post['post_text']);
-					$post_text = preg_replace("/\\[(\\/?)forumonly(:[a-zA-Z0-9]+)?\\]/i", '[\1hidden\2]', $post_text);
-					$post_text = preg_replace("/\\[(\\/?)siteonly(:[a-zA-Z0-9]+)?\\]/i", '[\1forumonly\2]', $post_text);
-				
-					$bbcode = new bbcode();
-					$bbcode->bbcode_second_pass($post_text, $first_post['bbcode_uid'], $first_post['bbcode_bitfield']);
-					$post_text = smiley_text($post_text);
-					?>
-					<div class="light">
-						<div class="head">
-							<a href="forums/viewtopic.php?f=<?= $_['phpbb']['news_forum_id'] ?>&t=<?= htmlspecialchars($topic['topic_id']) ?>"><h1><?= $topic['topic_title'] ?></h1></a>
-							<div class="subtitle">Posted by <a href="forums/memberlist.php?mode=viewprofile&u=<?= htmlspecialchars($topic['topic_poster']) ?>"><?= htmlspecialchars($topic['topic_first_poster_name']) ?></a> at <?= create_date($topic['topic_time']) ?></div>
-						</div>
-						<div class="body"><?= $post_text ?></div>
-					</div>
-					<?php
+				var loadingAnimationCycle = (function cycle() { 
+					if (loadingCounter > 0) {
+						$('#loadingwrapper').animate({ opacity: 1.0 }, 1500, 'linear', function() {
+							$('#loadingwrapper').animate({ opacity: 0.5 }, 1500, 'linear', cycle);
+						});
+					}
+				});
+
+				var showLoadingIndicator = function() {
+					if (++loadingCounter == 1) {
+						$('#loadingwrapper').show();
+						loadingAnimationCycle();
+					}
 				}
-				?>
+
+				var hideLoadingIndicator = function() {
+					if (--loadingCounter == 0) {
+						$('#loadingwrapper').hide();
+					}
+				}
+
+				var visiblePage = '';
+				var previousHash = window.location.hash;
+				var navigateToHash = function() {
+					var hash = window.location.hash;
+					var errorhash = previousHash;
+					previousHash = hash;
+					var parts = hash.split('/');
+					var url = 'ajax/home.php';
+					if (parts.length >= 1) {
+						if (parts[0] == '#apply') {
+							url = 'ajax/apply.php';
+						}
+					}
+					if (url == visiblePage) {
+						return;
+					}
+					$('#content').animate({left: '-1000px', opacity: 0.0}, 300, function() {
+						$('#content').css({left: '1000px'});
+					});
+					showLoadingIndicator();
+					$('#content').load(url, function(response, status, xhr) {
+						hideLoadingIndicator();
+						if (status == "error") {
+							if (xhr.status != 0) {
+								alert("Error (" + xhr.status + "): " + xhr.statusText);
+								window.location.hash = errorhash;
+							}
+						} else {
+							visiblePage = url;
+						}
+						$('#content').stop();
+						$('#content').animate({left: '0px', opacity: 1.0}, 300);
+					});						
+				};
+
+				$(window).on('hashchange', function() {
+					navigateToHash();
+				});
+				navigateToHash();
+			});
+		</script>
+
+		<div id="wrapper">	
+			<center>
+				<br />
+				<h1>準備中</h1>
+				<h1>Under Construction</h1>
+				<br />
+				<a href="forums"><b>The forums are up though!</b></a>
+				<br /><br />
+			</center>
+
+			<div id="content">
+				&nbsp;
+			</div>
+			
+			<div id="loadingwrapper">
+				<center>
+					<div id="loadingbox">
+						<ruby>
+							<rb>読み込み中</rb><rp>(</rp><rt>Loading</rt><rp>)</rp>
+						</ruby>
+					</div>
+				</center>
 			</div>
 		</div>
 		
