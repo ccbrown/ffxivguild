49a0dbbfbf619e82799cf1f6734a01e10e5a6be0
ccbrown <ccbrown112@gmail.com>
2013-10-07 18:24:14 -0700

    add application form

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -4,21 +4,23 @@ if (!defined('IN_FFXIVG')) { die(); }
 $config = array(
 
 	'phpbb'   => array(
-		'news_forum_id'    => '8',
-		'db_table_prefix'  => 'phpbb_',
+		'news_forum_id'          => '8',
+		'application_forum_id'   => '10',
+		'application_poster_id'  => '2',
+		'db_table_prefix'        => 'phpbb_',
 	),
 	
 	'page'   => array(
-		'date_format'     => 'M j, Y G:i A',
-		'time_offset'     => 0,
+		'date_format'            => 'M j, Y G:i A',
+		'time_offset'            => 0,
 	),
 	
 	'mysql'  => array(
-		'host'            => '',
-		'username'        => '',
-		'password'        => '',
-		'db_name'         => '',
-		'pconnect'        => false,
+		'host'                   => '',
+		'username'               => '',
+		'password'               => '',
+		'db_name'                => '',
+		'pconnect'               => false,
 	),
 );
 
